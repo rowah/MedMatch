551ec9821642ead6be602c7fb436f7da5f186b43
Rowa <jrowah@gmail.com>
2023-02-27 15:42:07 +0300

    alters table users for user names reg

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Thieth.MixProject do
   def application do
     [
       mod: {Thieth.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :bcrypt_elixir]
     ]
   end
 
